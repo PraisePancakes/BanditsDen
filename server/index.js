c24eafb9a7a671422db51593ad1b11abf4a0c673
PraisePancakes <100659653+PraisePancakes@users.noreply.github.com>
2023-10-11 01:32:17 -0400

    installed server-side dependencies and created base server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 
-app.listen(3001, () => {
-  console.log("Listening!");
-});
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, // Allow credentials (e.g., cookies, authorization headers)
+  allowedHeaders: ["Content-Type", "Authorization"], // Specify the allowed headers
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(cookieParser());
+
+const connectURI = process.env.CONNECT_URI;
+const portNum = process.env.PORT_NUMBER;
+
+mongoose
+  .connect(connectURI)
+  .then(console.log("SUCCESS DB CONNECT"))
+  .then(
+    app.listen(portNum, function (err) {
+      if (err) console.log("Error in server setup");
+      console.log("Server listening on Port", portNum);
+    })
+  );
